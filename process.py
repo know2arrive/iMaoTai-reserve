50cd282f192cef58ac9f8554ba54ef8ae5352af3
zerogau <zero_gau@126.com>
2024-07-01 10:51:32 +0800

    Update process.py

--- process.py
+++ process.py
@@ -335,6 +335,7 @@ def getUserEnergyAward(mobile: str):
     response = requests.post('https://h5.moutai519.com.cn/game/isolationPage/getUserEnergyAward', cookies=cookies,
                              headers=headers, json={})
     # response.json().get('message') if '无法领取奖励' in response.text else "领取奖励成功"
+    shadow_mobile = shadow(mobile)
     logging.info(
         f'领取耐力 : mobile:{shadow_mobile} :  response code : {response.status_code}, response body : {response.text}')
 
